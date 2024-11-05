794339aafb6c0c2179a0bc8c8dc79c30592e2474
AFreeChameleon <ben.evans@chameleo.dev>
2024-11-05 21:25:06 +0000

    Changed linux to deb

--- install/scripts/linux.sh
+++ install/scripts/linux.sh
@@ -3,7 +3,7 @@
 mkdir -p $HOME/.multi-tasker/bin
 curl https://afreechameleon.github.io/multask-docs/install/packages/multask_deb-0.20.0.tar.gz -s -o $PWD/mlt.tar.gz
 tar xvfz $PWD/mlt.tar.gz
-mv $PWD/mlt_linux-0.20.0 $HOME/.multi-tasker/bin
+mv $PWD/mlt_deb-0.20.0 $HOME/.multi-tasker/bin
 rm $PWD/mlt.tar.gz
 if [[ $SHELL ~= "zsh" ]]; then
     echo "export PATH=\"\$PATH:$HOME/.multi-tasker/bin\"" >> $HOME/.zshrc
