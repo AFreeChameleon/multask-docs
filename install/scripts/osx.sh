43fa129bc1d0f8c2a5498cadacd453a4b4df2806
Chameleo <55113649+AFreeChameleon@users.noreply.github.com>
2024-11-06 14:29:34 +0100

    Update osx.sh

--- install/scripts/osx.sh
+++ install/scripts/osx.sh
@@ -3,9 +3,9 @@
 mkdir -p $HOME/.multi-tasker/bin
 curl https://afreechameleon.github.io/multask-docs/install/packages/multask_osx-0.20.0.tar.gz -s -o $PWD/mlt.tar.gz
 tar xvfz $PWD/mlt.tar.gz
-mv $PWD/mlt_osx-0.20.0/mlt $HOME/.multi-tasker/bin
+mv $PWD/mult-app/mlt $HOME/.multi-tasker/bin
 rm $PWD/mlt.tar.gz
-rm -r $PWD/mlt_osx-0.20.0
+rm -r $PWD/mult-app
 if [[ $SHELL == *"zsh"* ]]; then
     rc_content=(cat $HOME/.zshrc)
     if [[ $rc_content == *"export PATH=\"\$PATH:$HOME/.multi-tasker/bin\""* ]]; then
