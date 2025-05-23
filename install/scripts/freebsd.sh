f375cbbebe7e2b6adc3a3dc12136e02a8210576e
AFreeChameleon <ben.evans@chameleo.dev>
2025-05-23 11:17:08 +0100

    Removed punctuation

--- install/scripts/freebsd.sh
+++ install/scripts/freebsd.sh
@@ -7,4 +7,4 @@ mv $PWD/multask_freebsd-0.20.0/mlt $HOME/.multi-tasker/bin
 rm $PWD/mlt.tar.gz
 rm -r $PWD/mlt_freebsd-0.20.0
 
-echo "To use multask, add this to your .rc file: \"export PATH=\"\$PATH:$HOME/.multi-tasker/bin\""
+echo "To use multask, add this to your .rc file: export PATH=\"\$PATH:$HOME/.multi-tasker/bin\"
